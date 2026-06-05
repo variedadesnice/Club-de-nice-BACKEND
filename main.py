95aad570c942b7074d72eef8bd055792b36dac6f
SebastianBaptista <sebastianpupe11@gmail.com>
2026-06-04 23:05:37 -0400

    Fix

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="Comunyapp API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
